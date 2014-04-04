2d5c5508a78fd3c3cd15953de9896323c37a7a61
Rob Beagrie <rob@beagrie.com>
2014-04-04 14:52:39 +0100

    Restructured gam_process to improve readability

--- gam_process.py
+++ gam_process.py
@@ -6,6 +6,7 @@ from doit.doit_cmd import DoitMain
 import argparse
 import os
 import sys
+import itertools
 
 
 parser = argparse.ArgumentParser(description='Map GAM-seq reads and create BigWigs and fastQC files')
@@ -17,92 +18,94 @@ parser.add_argument('-w','--window_sizes', metavar='WINDOW_SIZE', default=[1000,
 
 args = parser.parse_args()
 
-def get_mapping_tasks(input_fastq):
-    """Take the path to one bamfile and return the doit tasks which would convert it to a BigWig"""
-
+def with_extension(input_fastq, extension): 
     output_dir = os.path.dirname(input_fastq)
     basename = os.path.splitext(os.path.basename(input_fastq))[0]
+    return os.path.join(output_dir, basename + extension)
 
-    def with_extension(extension): return os.path.join(output_dir, basename + extension)
-
-    tasks_to_run = []
-
-    tasks_to_run.append({
-                "name"     : 'Mapping fastq',
+def map_fastq(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Mapping fastq',
                 "actions"  : ['bowtie2 -x genome -U %(dependencies)s | sed \'/XS : /d\' | samtools view -F 4 -bS - > %(targets)s'],
-                "targets"  : [with_extension(".bam")],
+                "targets"  : [with_extension(input_fastq, ".bam")],
                 "file_dep" : [input_fastq],
-                })
+              }
 
-    tasks_to_run.append({
-                "name"     : 'Sorting bam',
+def sort_bam(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Sorting bam',
                 "actions"  : ['samtools sort %(dependencies)s %(targets)s',
                               'mv %(targets)s.bam %(targets)s'],
-                "targets"  : [with_extension(".sorted.bam")],
-                "file_dep" : [with_extension(".bam")],
-                })
+                "targets"  : [with_extension(input_fastq, ".sorted.bam")],
+                "file_dep" : [with_extension(input_fastq, ".bam")],
+              }
 
-    tasks_to_run.append({
-                "name"     : 'Removing duplications',
+def remove_duplicates(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Removing duplications',
                 "actions"  : ['samtools rmdup -s %(dependencies)s %(targets)s',],
-                "targets"  : [with_extension(".rmdup.bam")],
-                "file_dep" : [with_extension(".sorted.bam")],
-                })
+                "targets"  : [with_extension(input_fastq, ".rmdup.bam")],
+                "file_dep" : [with_extension(input_fastq, ".sorted.bam")],
+              }
 
-    tasks_to_run.append({
-                "name"     : 'Converting bam to bedgraph',
+def index_bam(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Indexing bam',
+                "actions"  : ['samtools index %(dependencies)s',],
+                "targets"  : [with_extension(input_fastq, ".rmdup.bam.bai")],
+                "file_dep" : [with_extension(input_fastq, ".rmdup.bam")],
+              }
+
+def make_bedgraph(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Converting bam to bedgraph',
                 "actions"  : ['genomeCoverageBed -bg -ibam %(dependencies)s -g %(genome_file)s > %(targets)s'],
-                "targets"  : [with_extension(".bedgraph")],
-                "file_dep" : [with_extension(".rmdup.bam")],
-                })
+                "targets"  : [with_extension(input_fastq, ".bedgraph")],
+                "file_dep" : [with_extension(input_fastq, ".rmdup.bam")],
+              }
 
-    tasks_to_run.append({
-                "name"     : 'Converting bedgraph to bigwig',
+def make_bigwig(input_fastqs):
+    for input_fastq in input_fastqs:
+        yield {
+                "name"     : input_fastq,
+                "basename" : 'Converting bedgraph to bigwig',
                 "actions"  : ['bedGraphToBigWig %(dependencies)s %(genome_file)s %(targets)s'],
-                "targets"  : [with_extension(".bw")],
-                "file_dep" : [with_extension(".bedgraph")],
-                })
+                "targets"  : [with_extension(input_fastq, ".bw")],
+                "file_dep" : [with_extension(input_fastq, ".bedgraph")],
+              }
 
-    tasks_to_run.append({
-                'name'     : 'Indexing bam',
-                'actions'  : ['samtools index %(dependencies)s',],
-                'targets'  : [with_extension(".rmdup.bam.bai")],
-                'file_dep' : [with_extension(".rmdup.bam")],
-                })
+def get_coverage(args, window_sizes):
+    bamfiles = [ with_extension(input_fastq, ".rmdup.bam") for input_fastq in args.input_fastqs ]
+    for window_size in window_sizes:
+        yield {
+                'basename' : 'Getting coverage',
+                'name' :  '{0}bp windows'.format(window_size),
+                'actions' : ['echo "chrom start stop %(dependencies)s" > %(targets)s',
+                             'bash -i -c "multiBamCov -bams %(dependencies)s -bed <(bedtools makewindows -w {0} -g %(genome_file)s) >> %(targets)s"' .format(window_size)],
+                'targets' : [os.path.join(args.output_dir, 'coverage_at_{0}bp.multibam'.format(window_size))],
+                'file_dep' : bamfiles,
+                'task_dep' : 'Indexing bam',
+               }
 
-    for task in tasks_to_run:
-        task["name"] = task["name"] + " (%s)" % task["targets"][0]
-
-    return tasks_to_run
-
-def get_coverage_tasks(tasks_to_run, window, output_dir):
-    """Take the path to one bamfile and return the doit tasks which would convert it to a BigWig"""
-
-    bamfiles = []
-    for task in tasks_to_run:
-        if task['name'].startswith('Removing'):
-            bamfiles.extend(task['targets'])
-    
-    indexes = []
-    for task in tasks_to_run:
-        if task['name'].startswith('Indexing'):
-            indexes.append(task['name'])
-
-    tasks_to_run.append({
-            'name' : 'Getting coverage over {0}bp windows'.format(window),
-            'actions' : ['echo "chrom start stop %(dependencies)s" > %(targets)s',
-                         'bash -i -c "multiBamCov -bams %(dependencies)s -bed <(bedtools makewindows -w {0} -g %(genome_file)s) >> %(targets)s"' .format(window)],
-            'targets' : [os.path.join(output_dir, 'coverage_at_{0}bp.multibam'.format(window))],
-            'file_dep' : bamfiles,
-            'task_dep' : indexes,
-           })
-
-    tasks_to_run.append({
-            'name'     : 'Segmenting data at {0}bp'.format(window),
+def get_segmentation(args, window_sizes):
+    for window_size in window_sizes:
+        yield {
+            'basename' : 'Segmenting data',
+            'name'     : '{0}bp'.format(window_size),
             'actions'  : ['%(python_exec)s %(segmentation_script)s %(dependencies)s > %(targets)s'],
-            'targets'  : [os.path.join(output_dir, 'segmentation_at_{0}bp.multibam'.format(window))],
-            'file_dep' : [os.path.join(output_dir, 'coverage_at_{0}bp.txt'.format(window))],
-            })
+            'targets'  : [os.path.join(args.output_dir, 'segmentation_at_{0}bp.multibam'.format(window_size))],
+            'file_dep' : [os.path.join(args.output_dir, 'coverage_at_{0}bp.txt'.format(window_size))],
+            }
 
     """
     tasks_to_run.append({
@@ -111,9 +114,6 @@ def get_coverage_tasks(tasks_to_run, window, output_dir):
             'targets'  : [os.path.join(output_dir, '{0}bp_matrix.hdf5'.format(window))],
             'file_dep' : [os.path.join(output_dir, 'segmentation_at_{0}bp.multibam'.format(window))],
             })
-            """
-
-    return tasks_to_run
 
 def get_segmentation_bed_tasks(tasks_to_run, segmentation_file, window):
 
@@ -151,6 +151,7 @@ def get_segmentation_bed_tasks(tasks_to_run, segmentation_file, window):
                     })
 
     return tasks_to_run
+            """
 
 class MyTaskLoader(TaskLoader):
     def __init__(self, tasks_to_run, args):
@@ -191,15 +192,15 @@ class MyTaskLoader(TaskLoader):
 
 def main(args):
 
-    tasks_to_run = []
-    for input_fastq in args.input_fastqs:
-        tasks_to_run.extend( get_mapping_tasks(input_fastq))
-
-    for window in args.window_sizes:
-        tasks_to_run = get_coverage_tasks(tasks_to_run, window, args.output_dir)
-        segmentation_file = os.path.join(args.output_dir, 'segmentation_at_{0}bp.multibam'.format(window))
-
-        tasks_to_run = get_segmentation_bed_tasks(tasks_to_run, segmentation_file, window)
+    tasks_to_run = itertools.chain( map_fastq(args.input_fastqs),
+                                    sort_bam(args.input_fastqs),
+                                    remove_duplicates(args.input_fastqs),
+                                    index_bam(args.input_fastqs),
+                                    make_bedgraph(args.input_fastqs),
+                                    make_bigwig(args.input_fastqs),
+                                    get_coverage(args),
+                                    get_segmentation(args),
+                                  )
     
     sys.exit(DoitMain(MyTaskLoader(tasks_to_run, args)).run([]))
 
