81f29d8dd71f7e2bede7024637ef60bcd8a7b2d2
Rob Beagrie <rob@beagrie.com>
2014-04-30 18:09:38 +0200

    Use logging library to control debugging output.

--- pcc_from_npz.py
+++ pcc_from_npz.py
@@ -1,21 +1,31 @@
-from GamTools import corr
 import itertools
 import numpy as np
 import argparse
 import glob
+import logging
 
 parser = argparse.ArgumentParser(description='Calculate PCC between different chromosomes stored as npz files.')
 parser.add_argument('-p1','--paths1', metavar='FIRST_PATH', required=True, nargs='+', help='One or more input npz files.')
 parser.add_argument('-p2','--paths2', metavar='SECOND_PATH', required=True, nargs='+', help='One or more input npz files.')
 parser.add_argument('-d','--distance', metavar='DISTANCE', type=int, default=np.Inf, help='Only consider bins within this distance of each other.')
+parser.add_argument('--debug',
+    help='Print lots of debugging statements',
+    action="store_const",dest="loglevel",const=logging.DEBUG,
+    default=logging.WARNING
+)
+parser.add_argument('--verbose',
+    help='Be verbose',
+    action="store_const",dest="loglevel",const=logging.INFO
+)
+
 
 class ZipChroms(object):
     
     def __init__(self, path1, path2, distance=np.Inf):
         
-        print 'initializing'
+        logging.debug( 'initializing')
         self.distance = distance
-        print 'distance to consider is {0}'.format(self.distance)
+        logging.debug( 'distance to consider is {0}'.format(self.distance))
         self.path1, self.path2 = path1, path2
         self.loaded = False
         self.array1, self.array2 = None, None
@@ -23,7 +33,7 @@ class ZipChroms(object):
         
     def __iter__(self):
         if not self.loaded:
-            print 'loading data'
+            logging.debug( 'loading data')
             self.load_matrices()
         return self
         
@@ -33,7 +43,7 @@ class ZipChroms(object):
             try:
                 result = self.get_next()
             except StopIteration:
-                print 'Deleting'
+                logging.debug( 'Deleting')
                 del self.array1
                 del self.array2
                 self.loaded = False
@@ -126,7 +136,7 @@ def main(args):
         args.paths2 = sorted(glob.glob(args.paths2[0]))
 
     for paths in zip(args.paths1, args.paths2):
-        print '{0} <--> {1}'.format(*paths)
+        logging.info('{0} <--> {1}'.format(*paths))
 
     r = get_pcc(args.paths1, args.paths2, distance=args.distance)
 
@@ -135,6 +145,8 @@ def main(args):
 if __name__ == '__main__':
 
     args = parser.parse_args()
+
+    logging.basicConfig(level=args.loglevel)
 
     main(args)
 
