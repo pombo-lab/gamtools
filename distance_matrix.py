feb6f21560dd38f583b6db6c4b75557992519632
Rob Beagrie <rob@beagrie.com>
2014-04-30 18:08:35 +0200

    Don't overwrite existing distance matrices.

--- distance_matrix.py
+++ distance_matrix.py
@@ -1,6 +1,8 @@
 from GamTools import corr
 import numpy as np
 import argparse
+import os
+import sys
 
 parser = argparse.ArgumentParser(description='Calculate coverage over different window sizes for a list of bam files.')
 parser.add_argument('npz_frequencies_file', help='An npz file containing co-segregation frequencies to convert to correlations')
@@ -10,6 +12,9 @@ args = parser.parse_args()
 correlation_file = args.npz_frequencies_file.split('.')
 correlation_file[correlation_file.index('chrom')] = "corr"
 correlation_file = '.'.join(correlation_file)
+
+if os.path.exists(correlation_file):
+    sys.exit('File exists already, not overwriting')
 
 freqs = np.load(args.npz_frequencies_file)['freqs']
 
