081bc58a8a0499a72c95653cb694b27c51af113b
Rob Beagrie <rob@beagrie.com>
2014-06-04 17:25:45 +0100

    Added script to export npz to my5c

---
+++ npz_to_my5c.py
@@ -0,0 +1,17 @@
+import numpy as np
+import argparse
+import sys
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='Calculate coverage over different window sizes for a list of bam files.')
+parser.add_argument('-n', '--npz_frequencies_file', required=True, help='An npz file containing co-segregation frequencies to convert to correlations')
+
+args = parser.parse_args()
+
+def open_npz(fp):
+    handle = np.load(fp)
+    return handle['windows'], handle['scores']
+
+windows, data = open_npz(args.npz_frequencies_file)
+names = [ '{}:{}-{}'.format(*i) for i in windows ]
+pd.DataFrame(data, index=names, columns=names).to_csv(sys.stdout, sep='\t')
