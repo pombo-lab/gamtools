29900064c8aea5faa9e5aa4b88db984d94704158
Rob Beagrie <rob@beagrie.com>
2014-06-04 11:49:06 +0100

    Use new GamTools functions to convert frequencies to matrices

--- distance_matrix.py
+++ distance_matrix.py
@@ -1,29 +1,37 @@
-from GamTools import corr
+from GamTools.segmentation import map_freqs
+from GamTools.cosegregation import D, Dprime, corr
 import numpy as np
 import argparse
 import os
 import sys
 
 parser = argparse.ArgumentParser(description='Calculate coverage over different window sizes for a list of bam files.')
-parser.add_argument('npz_frequencies_file', help='An npz file containing co-segregation frequencies to convert to correlations')
+parser.add_argument('-n', '--npz_frequencies_file', required=True, help='An npz file containing co-segregation frequencies to convert to correlations')
+parser.add_argument('-m', '--method', default='Dprime', help='An npz file containing co-segregation frequencies to convert to correlations')
 
 args = parser.parse_args()
 
+methods = { 'D' : D,
+            'Dprime' : Dprime,
+            'corr' : corr,
+          }
+
 correlation_file = args.npz_frequencies_file.split('.')
-correlation_file[correlation_file.index('chrom')] = "corr"
+correlation_file[correlation_file.index('freqs')] = args.method
 correlation_file = '.'.join(correlation_file)
 
 if os.path.exists(correlation_file):
     sys.exit('File exists already, not overwriting')
 
-freqs = np.load(args.npz_frequencies_file)['freqs']
+method = methods[args.method]
 
-def flatten_freqs(freqs):
-        
-    freqs_shape = freqs.shape
-    flat_shape = ( freqs_shape[0] * freqs_shape[1], freqs_shape[2], freqs_shape[3])
-    return freqs.reshape(flat_shape)
+npz_data = np.load(args.npz_frequencies_file)
 
-distances = np.array(map(corr, flatten_freqs(freqs))).reshape(freqs.shape[:2])
+windows = npz_data['windows']
+freqs = npz_data['freqs']
 
-np.savez_compressed(correlation_file, corr=distances)
+distances = map_freqs(method, freqs)
+
+np.savez_compressed(correlation_file, 
+                    windows=windows,
+                    scores=distances)
