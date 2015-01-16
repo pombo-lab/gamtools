5ae6239c971899314bc8c96eda26396bdf4245f5
Rob Beagrie <rob@beagrie.com>
2015-01-16 15:24:22 +0100

    Don't permute unmappable regions, mtDNA or unplaced contigs.

--- circular_permute_segmentation.py
+++ circular_permute_segmentation.py
@@ -1,25 +1,47 @@
 import numpy as np
 import argparse
 import pandas as pd
+from GamTools.segmentation import open_segmentation
 import sys
 
 parser = argparse.ArgumentParser(description='Circularly permute every column of a segmentation file')
 parser.add_argument('segmentation_file', help='A segmentation file to use as input')
 
 def permute_data(input_data):
-    no_windows, no_samples = input_data.shape
+
+    # Only permute positions that were mapped at least once
+    mappable = input_data.sum(axis=1).astype(bool)
+    no_windows, no_samples = input_data[mappable].shape
+
+    # Make a copy of the original data
     permutation = input_data.copy()
+
+    # Loop over columns
     for i in range(no_samples):
+
+        # Choose a random position to break the segmentation in two,
+        # Swap the two chunks around and write them to the copied df
         offset = np.random.randint(no_windows)
-        permutation.iloc[:,i] = list(pd.concat([input_data.iloc[offset:,i], input_data.iloc[:offset,i]]))
+        
+        new_start = input_data[mappable].iloc[offset:,i]
+        new_end = input_data[mappable].iloc[:offset,i]
+        new_col = list(pd.concat([new_start, new_end]))
+        
+        permutation.ix[mappable,i] = new_col
     return permutation
 
 if __name__ == '__main__':
 
     args = parser.parse_args()
     
-    segmentation = pd.read_csv(args.segmentation_file,
-                          delim_whitespace=True, index_col=[0,1,2])
+    segmentation = open_segmentation(args.segmentation_file)
+
+    # Sex chromosomes, unjoined contigs and mtDNA behave weirdly,
+    # so don't permute them into the autosomal regions.
+
+    autosomes = ['chr{0}'.format(c) for c in range(1,20)]
+    
+    segmentation = segmentation.loc[autosomes]
 
     permutation = permute_data(segmentation)
 
