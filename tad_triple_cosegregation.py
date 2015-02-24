dd3578b63f84b128b20d81e7376a586caee486cf
Rob Beagrie <rob@beagrie.com>
2015-02-24 17:43:15 +0100

    Use cython functions for tad triple cosegregation script.

--- tad_triple_cosegregation.py
+++ tad_triple_cosegregation.py
@@ -4,42 +4,11 @@ import time
 import numpy as np
 import itertools
 from GamTools import segmentation
-
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is one of "yes" or "no".
-    """
-    valid = {"yes":True,   "y":True,  "ye":True,
-             "no":False,     "n":False}
-    if default == None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "\
-                             "(or 'y' or 'n').\n")
+from GamTools.coseg_3 import coseg_all_3
 
 def get_npz_path(args):
 
-    return args.segmentation_file + '.tads_dprime.' + args.region
+    return args.segmentation_file + '.tads_dprime.' + '^'.join(args.regions)
 
 def tad_name(row):
     return '{0}:{1}-{2}'.format(row.chrom, row.start, row.stop)
@@ -49,40 +18,60 @@ def chrom_main(args):
     data = segmentation.open_segmentation(args.segmentation_file)
     tads = segmentation.open_segmentation(args.tad_segmentation_file)
 
-    tad_list = segmentation.region_from_location_string(
-        tads, args.region).reset_index()[['chrom', 'start', 'stop']]
+    def get_tads(loc):
+        
+        chrom = loc.split(':')[0]
+        
+        chrom_df = segmentation.region_from_location_string(tads, chrom).reset_index()[['chrom', 'start', 'stop']].reset_index()
+        chrom_df['name'] = chrom_df.apply(tad_name, axis=1)
+        chrom_df = chrom_df.set_index(['chrom', 'start', 'stop'])
+        
+        tad_strings = segmentation.region_from_location_string(chrom_df, loc)
+        
+        return tad_strings.itertuples(index=False)
 
-    tad_strings = list(tad_list.apply(tad_name, axis=1))
+    def get_region_tad_combs(loc1, loc2, loc3):
+            
+        tads1, tads2, tads3 = [get_tads(loc) for loc in 
+                    (loc1, loc2, loc3)]
 
-    print 'starting calculation for', args.region
+        tad_combs = itertools.product(tads1, tads2, tads3)
+
+        uniq_combs = list(set([tuple(sorted(comb)) for comb in tad_combs]))
+
+        return uniq_combs
+
+    tad_combinations = get_region_tad_combs(*args.regions)
+
+    print 'starting calculation for', '^'.join(args.regions)
     start_time = time.clock()
 
-    tad_matrix = np.zeros((len(tad_strings),)*3) * np.NaN
+    result = []
+    res_append = result.append
 
-    for tad_indexes in itertools.combinations(range(len(tad_strings)),3):
+    for tad_indexes, tad_strings in [zip(*comb) for comb in tad_combinations]:
 
-        tad1, tad2, tad3 = tad_indexes
-        
-        tad_coseg = segmentation.get_matrix_from_regions(
-            segmentation.region_from_location_string(data, tad_strings[tad1]),
-            segmentation.region_from_location_string(data, tad_strings[tad2]),
-            segmentation.region_from_location_string(data, tad_strings[tad3]))
-        
+        if len(set(tad_indexes)) != 3:
+            continue
+
+        tad_data = [np.array(segmentation.region_from_location_string(data, tad_str)).astype(int) for
+                    tad_str in tad_strings]
+
+        tad_coseg = coseg_all_3(*tad_data)
+
         tads_value = np.nanmean(tad_coseg)
         
-        for index_comb in itertools.permutations(tad_indexes):
-            
-            tad_matrix[index_comb] = tads_value
+        res_append(list(tad_indexes) + [tads_value])
 
     npz_path = get_npz_path(args)
-    np.savez_compressed(npz_path, dprime=tad_matrix, windows=np.array(tad_strings))
+    np.savez_compressed(npz_path, dprime=result)
 
-    print 'region size is: {0} x {1}'.format(*tad_matrix.shape), 
+    print 'region size is: {0}'.format(len(result)), 
     print 'Calculation took {0}s'.format(time.clock() - start_time)
     print 'Done!'
 
 parser = argparse.ArgumentParser(description='Calculate coverage over different window sizes for a list of bam files.')
-parser.add_argument('-r','--region', metavar='REGION', required=True, help='Specific genomic region to calculate matrices for')
+parser.add_argument('-r','--regions', metavar='REGION', nargs='+', required=True, help='Specific genomic regions to calculate matrices for')
 parser.add_argument('-s', '--segmentation-file', required=False, help='A segmentation file to use as input')
 parser.add_argument('-t', '--tad-segmentation-file', required=False, help='A tad segmentation file to use as input')
 parser.set_defaults(func=chrom_main)
@@ -90,6 +79,8 @@ parser.set_defaults(func=chrom_main)
 if __name__ == "__main__":
 
     args = parser.parse_args()
+
+    assert len(args.regions) == 3
 
     chrom_main(args)
 
