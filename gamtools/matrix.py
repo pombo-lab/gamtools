f0bc09d87f6c06d95539f80eef2ae0f2f7f1a7c8
Rob Beagrie <rob@beagrie.com>
2016-05-03 17:24:46 +0100

    Started documenting matrix

--- gamtools/matrix.py
+++ gamtools/matrix.py
@@ -33,29 +33,59 @@ def read_npz(filepath):
     """
 
     handle = np.load(filepath)
-    contact_matrix = handle['scores']
+    proximity_matrix = handle['scores']
 
     try:
-        windows = [handle['windows_{}'.format(i)] for i in range(contact_matrix.ndim)]
+        windows = [handle['windows_{}'.format(i)] for i in range(proximity_matrix.ndim)]
     except KeyError:
         windows = [handle['windows'], handle['windows']]
 
-    return windows, contact_matrix
+    return windows, proximity_matrix
 
 def read_txt(filepath, compression='infer'):
+    """Open an txt file containing a proximity matrix
 
-    contact_matrix = pd.read_csv(filepath, compression=compression, sep='\t', index_col=0)
+    :param str filepath: Path to the txt file
+    :param str compression: One of {'infer', 'gzip', 'bz2', None}, \
+    default 'infer'. For on-the-fly decompression of on-disk data. If \
+    'infer', then use gzip or bz2 if filepath is a string \
+    ending in '.gz' or '.bz2', respectively, and no decompression \
+    otherwise. Set to None for no decompression.
+    :returns: (list of genomic locations for x-axis, list of \
+    genomic locations for y-axis), \
+            :class:`numpy array <numpy.ndarray>` proximity matrix.
+    """
 
-    windows_0 = windows_from_name_strings(contact_matrix.index)
-    windows_1 = windows_from_name_strings(contact_matrix.columns)
+    proximity_matrix = pd.read_csv(filepath, compression=compression, sep='\t', index_col=0)
 
-    return (windows_0, windows_1), contact_matrix
+    windows_0 = windows_from_name_strings(proximity_matrix.index)
+    windows_1 = windows_from_name_strings(proximity_matrix.columns)
+
+    return (windows_0, windows_1), proximity_matrix
 
 def read_zipped_txt(filepath):
+    """Open a gzipped txt file containing a proximity matrix
+
+    :param str filepath: Path to the gzip compressed txt file
+    :returns: (list of genomic locations for x-axis, list of \
+    genomic locations for y-axis), \
+            :class:`numpy array <numpy.ndarray>` proximity matrix.
+    """
 
     return read_txt(filepath, compression='gzip')
 
 def read_windows(filepath, chrom):
+    """Retrieve the genomic locations (windows) covering a given chromosome
+    from a text file.
+
+    This function is used when opening Pi matrices output from SLICE, as
+    these matrices do not have embedded window locations.
+
+    :param str filepath: Path to the text file
+    :param str chrom: Chromosome to fetch the windows for.
+    :returns: (list of genomic locations for x-axis, list of \
+    genomic locations for y-axis)
+    """
 
     data = pd.read_csv(filepath, delim_whitespace=True, header=None)
 
@@ -64,19 +94,31 @@ def read_windows(filepath, chrom):
     return [windows, windows]
 
 def read_triangular(filepath):
+    """Open Pi matrix output from SLICE.
+
+    All matrix opening functions return first the
+    genomic windows corresponding to the axes of the
+    proximity matrix, then the proximity matrix itself.
+    Since SLICE output matrices do not embed the genomic
+    locations of the windows, the first return value is
+    None.
+
+    :param str filepath: Path to the SLICE output file
+    :returns: (None, SLICE Pi matrix)
+    """
 
     with open(filepath) as in_data:
         arr = [[float(i) for i in line.split()] for line in in_data]
     N = len(arr[-1])
-    contact_matrix = np.zeros((N,N))
-    lower_i = np.tril_indices_from(contact_matrix)
-    upper_i = np.triu_indices_from(contact_matrix)
-    contact_matrix[:] = np.NAN
-    contact_matrix[lower_i] = list(itertools.chain(*arr))
-    contact_matrix[upper_i] = contact_matrix.T[upper_i]
-    contact_matrix[contact_matrix > 1.] = np.NAN
+    proximity_matrix = np.zeros((N,N))
+    lower_i = np.tril_indices_from(proximity_matrix)
+    upper_i = np.triu_indices_from(proximity_matrix)
+    proximity_matrix[:] = np.NAN
+    proximity_matrix[lower_i] = list(itertools.chain(*arr))
+    proximity_matrix[upper_i] = proximity_matrix.T[upper_i]
+    proximity_matrix[proximity_matrix > 1.] = np.NAN
 
-    return None, contact_matrix
+    return None, proximity_matrix
 
 
 input_formats = {
@@ -88,29 +130,36 @@ input_formats = {
         #'csv': '',
 }
 
-def write_txt(windows, contact_matrix, output_file):
+def write_txt(windows, proximity_matrix, output_file):
+    """Write a proximity matrix to a txt file.
 
-    if contact_matrix.ndim != 2:
+    :param tuple windows: (list of x-axis windows, list of y-axis windows)
+    :param str proximity_matrix: Path to the SLICE output file
+    :param str filepath: Path to the SLICE output file
+    :returns: (None, SLICE Pi matrix)
+    """
+
+    if proximity_matrix.ndim != 2:
         raise NotImplementedError('Plain text output is only supported for 2 dimensional matrices. Please try saving as an npz file.')
 
     names_0, names_1 = [get_name_strings(axis_windows) for axis_windows in windows]
 
-    pd.DataFrame(contact_matrix, index=names_0, columns=names_1).to_csv(output_file, sep='\t', na_rep="NaN")
+    pd.DataFrame(proximity_matrix, index=names_0, columns=names_1).to_csv(output_file, sep='\t', na_rep="NaN")
 
-def write_zipped_txt(windows, contact_matrix, output_file):
+def write_zipped_txt(windows, proximity_matrix, output_file):
 
     import gzip
     with gzip.open(output_file, 'wb', compresslevel=5) as zipped_output:
-        write_txt(windows, contact_matrix, zipped_output)
+        write_txt(windows, proximity_matrix, zipped_output)
 
-def write_npz(windows, contact_matrix, output_file):
+def write_npz(windows, proximity_matrix, output_file):
 
     window_dict = {'windows_{}'.format(i):win for i,win in enumerate(windows)}
-    np.savez_compressed(output_file, scores=contact_matrix, **window_dict)
+    np.savez_compressed(output_file, scores=proximity_matrix, **window_dict)
 
-def write_csv(windows, contact_matrix, output_file):
+def write_csv(windows, proximity_matrix, output_file):
 
-        interactions_df = pd.DataFrame(contact_matrix).unstack()
+        interactions_df = pd.DataFrame(proximity_matrix).unstack()
         interactions_df = interactions_df[interactions_df > 0]
         interactions_df =interactions_df.reset_index()
         interactions_df.columns = ['Pos_A', 'Pos_B', 'interaction']
@@ -120,11 +169,11 @@ def write_csv(windows, contact_matrix, output_file):
         output_cols = ['chrom', 'Pos_A', 'Pos_B', 'dist', 'interaction']
         interactions_df[output_cols].to_csv(output_file, sep='\t', index=False)
 
-def write_zipped_csv(windows, contact_matrix, output_file):
+def write_zipped_csv(windows, proximity_matrix, output_file):
 
     import gzip
     with gzip.open(output_file, 'wb', compresslevel=5) as zipped_output:
-        write_csv(windows, contact_matrix, zipped_output)
+        write_csv(windows, proximity_matrix, zipped_output)
 
 output_formats = {
         'npz': write_npz,
@@ -156,16 +205,16 @@ def detect_file_type(file_name):
     raise TypeError('Extension "{}" not recognized'.format(file_ext))
 
 
-def check_windows(contact_matrix, windows):
+def check_windows(proximity_matrix, windows):
 
     windows_sizes = [len(win) for win in windows]
-    for i in range(contact_matrix.ndim):
-        if contact_matrix.shape[i] != windows_sizes[i]:
+    for i in range(proximity_matrix.ndim):
+        if proximity_matrix.shape[i] != windows_sizes[i]:
             raise ValueError(
                 'Contact matrix size ({}) does not match the number '
                 'of windows supplied ({}). Please check you have '
                 'specified the correct region.'.format(
-                    ' x '.join([str(s) for s in contact_matrix.shape]),
+                    ' x '.join([str(s) for s in proximity_matrix.shape]),
                     ' x '.join([str(s) for s in windows_sizes])))
 
 
@@ -184,22 +233,22 @@ def kth_diag_indices(a, k):
             return rows, cols
 
 
-def apply_threshold(contact_matrix, thresholds):
+def apply_threshold(proximity_matrix, thresholds):
 
-    out_matr = np.zeros_like(contact_matrix)
+    out_matr = np.zeros_like(proximity_matrix)
 
-    for d in range(1, contact_matrix.shape[0]+1):
-        contact_matrix_diag = contact_matrix.diagonal(d).copy()
+    for d in range(1, proximity_matrix.shape[0]+1):
+        proximity_matrix_diag = proximity_matrix.diagonal(d).copy()
 
         try:
             thresh = np.array(thresholds.loc[d])
         except KeyError:
             thresh = np.array(thresholds.iloc[-1])
 
-        below_thresh = contact_matrix_diag < thresh
-        contact_matrix_diag[below_thresh] = 0.
-        out_matr[kth_diag_indices(out_matr, d)] = contact_matrix_diag
-        out_matr[kth_diag_indices(out_matr, -d)] = contact_matrix_diag
+        below_thresh = proximity_matrix_diag < thresh
+        proximity_matrix_diag[below_thresh] = 0.
+        out_matr[kth_diag_indices(out_matr, d)] = proximity_matrix_diag
+        out_matr[kth_diag_indices(out_matr, -d)] = proximity_matrix_diag
 
     return out_matr
 
@@ -208,7 +257,7 @@ def convert(input_file, input_format,
             output_file, output_format,
             windows=None, thresholds=None):
 
-    _windows, contact_matrix = input_formats[input_format](input_file)
+    _windows, proximity_matrix = input_formats[input_format](input_file)
 
     if input_format == 'triangular':
         if windows is None:
@@ -219,12 +268,12 @@ def convert(input_file, input_format,
 
         _windows = windows
 
-    check_windows(contact_matrix, _windows)
+    check_windows(proximity_matrix, _windows)
 
     if thresholds is not None:
-        contact_matrix = apply_threshold(contact_matrix, thresholds)
+        proximity_matrix = apply_threshold(proximity_matrix, thresholds)
 
-    output_formats[output_format](_windows, contact_matrix, output_file)
+    output_formats[output_format](_windows, proximity_matrix, output_file)
 
 def convert_from_args(args):
 
