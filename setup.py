33729684c0ef2a724fbe35f93f76a270f0d86c11
Robert Beagrie <rob@beagrie.co.uk>
2022-10-24 16:18:38 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def read(fname):
 
 setup(
     name = "gamtools",
-    version = "2.0.0alpha6",
+    version = "2.0.0alpha7",
     author = "Rob Beagrie",
     author_email = "rob@beagrie.co.uk",
     url = "https://gam.tools",
