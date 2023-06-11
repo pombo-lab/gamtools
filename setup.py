a814e24bcb4a58c2a8800cbd5fb116c89ff76830
Robert Beagrie <rob@beagrie.co.uk>
2023-06-11 19:15:15 +0100

    Include numpy headers

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ import numpy
 
 extensions = [
     Extension('gamtools.cosegregation_internal',
-        ["lib/gamtools/cosegregation_internal.pyx"]),
+        ["lib/gamtools/cosegregation_internal.pyx"],
+              include_dirs=[numpy.get_include()]),
     Extension('gamtools.mirnylib_numutils_internal',
-        ["lib/gamtools/mirnylib_numutils_internal.pyx"])
+        ["lib/gamtools/mirnylib_numutils_internal.pyx"],
+              include_dirs=[numpy.get_include()]),
 	   ]
  
 setup(
-    ext_modules = cythonize(extensions),
+    ext_modules = cythonize(extensions, language_level = "3"),
 )
